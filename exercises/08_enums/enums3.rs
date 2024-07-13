1021784e5d750b00c86e20f2f99f5d15d3d4125d
David Asabina <vid@bina.me>
2024-07-13 14:58:37 +0200

    chore: Implement enums and mutate self

--- exercises/08_enums/enums3.rs
+++ exercises/08_enums/enums3.rs
@@ -5,10 +5,11 @@
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 enum Message {
-    // TODO: implement the message variant types based on their usage below
+    ChangeColor(u8, u8, u8),
+    Quit,
+    Move(Point),
+    Echo(String),
 }
 
 struct Point {
@@ -44,6 +45,12 @@ impl State {
         // TODO: create a match expression to process the different message variants
         // Remember: When passing a tuple as a function argument, you'll need extra parentheses:
         // fn function((t, u, p, l, e))
+        match message {
+            Message::ChangeColor(a, b, c) => self.color = (a, b, c),
+            Message::Quit => self.quit = true,
+            Message::Move(p) => self.position = p,
+            Message::Echo(m) => self.message = m
+        }
     }
 }
 
