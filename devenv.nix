7c3edf0b0596563bf97cf778cd46e400c54a385b
David Asabina <vid@bina.me>
2024-10-10 21:13:05 +0900

    fix: Resolve framework not found CoreFoundation

--- devenv.nix
+++ devenv.nix
@@ -5,4 +5,9 @@
     enable = true;
     channel = "stable";
   };
+
+  packages = with pkgs; [
+  ] ++ lib.optionals pkgs.stdenv.isDarwin [
+    pkgs.darwin.apple_sdk.frameworks.CoreServices
+  ];
 }
